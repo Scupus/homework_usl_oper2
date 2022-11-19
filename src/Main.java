552c5939110c1c6cee80d3a52ff322f5c85adec0
Alex <loalantref@hmail.com>
2022-11-19 20:50:32 +1200

    Часть 1

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        int clientOS =0;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+
+        System.out.println("________________________________________________________________________");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
